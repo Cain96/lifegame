202a2dd3e031d23644f00bfb330866f2010c3edb
Kenta Kuroda <tk.777.k@gmail.com>
2016-10-27 01:13:51 +0900

    Make the view of board

--- src/lifegame/BoardView.java
+++ src/lifegame/BoardView.java
@@ -8,10 +8,9 @@ import java.awt.Graphics;
  */
 public class BoardView extends JPanel {
 
-	int c, r;
-	private Settings settings;
+	int c, r, w, h, interval;
 
-	public BoardView(Settings settings){
+	public BoardView(Settings settings) {
 		c = settings.getCols();
 		r = settings.getRows();
 	}
@@ -20,10 +19,25 @@ public class BoardView extends JPanel {
 	public void paint(Graphics g) {
 		super.paint(g);
 
-		for (int i = 0; i < c + 1; i++) {
-			g.drawLine(20, 20 * i, 20 * (c + 1), 20 * i);
-			g.drawLine(20 * (i + 1), 0, 20 * (i + 1), 20 * r);
-		}
+		w = this.getWidth() - 1;
+		h = this.getHeight() - 1;
 
+		interval = widthHeightCheck(w / r, h / c);
+
+		for (int i = 0; i < c + 1; i++) {
+			g.drawLine(0, interval * i, interval * r, interval * i); //横線
+		}
+		for (int i = 0; i < r + 1; i++) {
+			g.drawLine(interval * i, 0, interval * i, interval * c); //縦線
+		}
+	}
+
+	public int widthHeightCheck(int w, int h) {
+
+		if (w > h) {
+			return h;
+		} else {
+			return w;
+		}
 	}
 }
