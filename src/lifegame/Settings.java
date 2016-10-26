ceed4895aab8069a68ca813205a16c37c6c45245
Kenta Kuroda <tk.777.k@gmail.com>
2016-10-26 11:13:43 +0900

    Create Settings

---
+++ src/lifegame/Settings.java
@@ -0,0 +1,17 @@
+package lifegame;
+
+/**
+ * Created by kuro on 2016/10/25.
+ */
+public class Settings {
+	private int cols = 10;
+	private int rows = 10;
+
+	public int getCols() {
+		return cols;
+	}
+
+	public int getRows() {
+		return rows;
+	}
+}
