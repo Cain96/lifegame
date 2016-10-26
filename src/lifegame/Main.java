80061e6493ec38c959f75021245c1a709d252aaa
Kenta Kuroda <tk.777.k@gmail.com>
2016-10-26 11:27:05 +0900

    Add isUndoable to Main

--- src/lifegame/Main.java
+++ src/lifegame/Main.java
@@ -6,25 +6,25 @@ package lifegame;
 public class Main {
 	private static Settings settings;
 
-	public static void main (String args[]){
+	public static void main(String args[]) {
 		settings = new Settings();
 
 		BoardModel model = new BoardModel(settings.getCols(), settings.getRows());
 		model.addlistener(new ModelPrinter());
 
-		model.changeCellState(1,1);
-		model.changeCellState(2,2);
-		model.changeCellState(0,3);
-		model.changeCellState(1,3);
-		model.changeCellState(2,3);
-		model.changeCellState(4,4);
-		model.changeCellState(4,4);
+		model.changeCellState(1, 1);
+		model.changeCellState(2, 2);
+		model.changeCellState(0, 3);
+		model.changeCellState(1, 3);
+		model.changeCellState(2, 3);
+		model.changeCellState(4, 4);
+		model.changeCellState(4, 4);
 
-		for(int i=0; i<30; ++i) {
+		for (int i = 0; i < 30; ++i) {
 			model.next();
 		}
 
-		for (int i=0; i<35; ++i) {
+		while (model.isUndoable()) {
 			model.undo();
 		}
 	}
