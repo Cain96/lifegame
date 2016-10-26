6c94094f4e15a77ec5737253bc14a07a64f5be79
Kenta Kuroda <tk.777.k@gmail.com>
2016-10-26 11:44:34 +0900

    Make GUI window

--- src/lifegame/Main.java
+++ src/lifegame/Main.java
@@ -1,14 +1,20 @@
 package lifegame;
 
+import javax.swing.*;
+import java.awt.*;
+
 /**
  * Created by kuro on 2016/10/13.
  */
-public class Main {
+public class Main implements Runnable {
 	private static Settings settings;
 
 	public static void main(String args[]) {
-		settings = new Settings();
+		SwingUtilities.invokeLater(new Main());
+	}
 
+	public void run() {
+		settings = new Settings();
 		BoardModel model = new BoardModel(settings.getCols(), settings.getRows());
 		model.addlistener(new ModelPrinter());
 
@@ -19,6 +25,18 @@ public class Main {
 		model.changeCellState(2, 3);
 		model.changeCellState(4, 4);
 		model.changeCellState(4, 4);
-		
+
+		//ウィンドウの作成
+		JFrame frame = new JFrame();
+		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		frame.setTitle("Lifegame");
+
+		//ウィンドウ内部のベースパネルの作成
+		JPanel base = new JPanel();
+		frame.setPreferredSize(new Dimension(400, 300)); //希望サイズの指定
+		frame.setMinimumSize(new Dimension(300, 200)); //最小サイズの指定
+
+		frame.pack();
+		frame.setVisible(true);
 	}
 }
