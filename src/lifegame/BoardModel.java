ec56c2c2540c533ee5d1094a2e11a60fc90fe4ec
Kenta Kuroda <tk.777.k@gmail.com>
2016-10-26 11:18:54 +0900

    Create changeCellsState and printForDebug method

--- src/lifegame/BoardModel.java
+++ src/lifegame/BoardModel.java
@@ -6,7 +6,8 @@ package lifegame;
 public class BoardModel {
 	private int cols;
 	private int rows;
-	private boolean[][] cells;
+	private int oldCounter=0;
+	private boolean[][] cells = new boolean[rows][cols];
 
 	public BoardModel(int c, int r) {
 		cols = c;
@@ -22,16 +23,36 @@ public class BoardModel {
 		return rows;
 	}
 
+	//Debug用盤面の状態を出力するmethod
 	public void printForDebug() {
-		for (int i = 0; i < cells.length; i++){
-			for(int j = 0; j < cells[0].length; j++){
-				if(!cells[i][j]){
+		for (int i = 0; i < cells.length; i++) {
+			for (int j = 0; j < cells[0].length; j++) {
+				if (!cells[i][j]) {
 					System.out.print(".");
-				}else{
+				} else {
 					System.out.print("*");
 				}
 			}
 			System.out.println();
+		}
+	}
+
+	//指定されたセルの状態を変更するmethod
+	public void changeCellState(int x, int y) {
+			addOldCells();
+		cells[y][x] = !cells[y][x];
+		fireUpdate();
+	}
+
+	//listenersに要素を追加するmethod
+	public void addlistener(BoardListener listener) {
+		listeners.add(listener);
+	}
+
+	//listenerのupdateを行うためのmethod
+	private void fireUpdate() {
+		for (BoardListener listener : listeners) {
+			listener.updated(this);
 		}
 	}
 }
--- src/lifegame/Main.java
+++ src/lifegame/Main.java
@@ -4,8 +4,20 @@ package lifegame;
  * Created by kuro on 2016/10/13.
  */
 public class Main {
+	private static Settings settings;
+
 	public static void main (String args[]){
-		BoardModel model = new BoardModel(10, 10);
-		model.printForDebug();
+		settings = new Settings();
+
+		BoardModel model = new BoardModel(settings.getCols(), settings.getRows());
+		model.addlistener(new ModelPrinter());
+
+		model.changeCellState(1,1);
+		model.changeCellState(2,2);
+		model.changeCellState(0,3);
+		model.changeCellState(1,3);
+		model.changeCellState(2,3);
+		model.changeCellState(4,4);
+		model.changeCellState(4,4);
 	}
 }
